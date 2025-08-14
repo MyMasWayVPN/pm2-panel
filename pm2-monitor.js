8e11e578c62913e1416dba9067faae77aa51700b
MyMasWayVPN <107555389+MyMasWayVPN@users.noreply.github.com>
2025-08-15 04:08:24 +0700

    Update pm2-monitor.js

--- pm2-monitor.js
+++ pm2-monitor.js
@@ -1,66 +1,63 @@
-// pm2-monitor.js
 const express = require('express');
-const http = require('http');
-const socketIO = require('socket.io');
 const pm2 = require('pm2');
-const { spawn } = require('child_process');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const app = express();
-const server = http.createServer(app);
-const io = socketIO(server);
 const PORT = 4000;
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+// Get list of processes
+app.get('/api/list', (req, res) => {
+    pm2.connect(err => {
+        if (err) return res.status(500).send(err.message);
+        pm2.list((err, list) => {
+            if (err) return res.status(500).send(err.message);
+            res.json(list);
+            pm2.disconnect();
+        });
+    });
 });
 
-io.on('connection', (socket) => {
-    console.log('Client terhubung');
+// Control actions: start, stop, restart, delete
+app.post('/api/action', (req, res) => {
+    const { action, name } = req.body;
+    pm2.connect(err => {
+        if (err) return res.status(500).send(err.message);
 
-    // Kirim data monitoring setiap 3 detik
-    const sendPM2Data = () => {
-        pm2.connect(err => {
-            if (err) {
-                socket.emit('pm2-error', err.message);
-                return;
-            }
-            pm2.list((err, list) => {
-                if (err) {
-                    socket.emit('pm2-error', err.message);
-                } else {
-                    socket.emit('pm2-data', list);
-                }
+        switch (action) {
+            case 'stop':
+                pm2.stop(name, () => { res.json({ status: 'stopped' }); pm2.disconnect(); });
+                break;
+            case 'start':
+                pm2.start(name, () => { res.json({ status: 'started' }); pm2.disconnect(); });
+                break;
+            case 'restart':
+                pm2.restart(name, () => { res.json({ status: 'restarted' }); pm2.disconnect(); });
+                break;
+            case 'delete':
+                pm2.delete(name, () => { res.json({ status: 'deleted' }); pm2.disconnect(); });
+                break;
+            default:
+                res.status(400).json({ error: 'Invalid action' });
                 pm2.disconnect();
-            });
-        });
-    };
-
-    sendPM2Data();
-    const interval = setInterval(sendPM2Data, 3000);
-
-    // Menerima request log dari client
-    socket.on('get-logs', (appName) => {
-        const logProcess = spawn('pm2', ['logs', appName, '--lines', '20', '--raw']);
-
-        logProcess.stdout.on('data', (data) => {
-            socket.emit('pm2-log', data.toString());
-        });
-
-        logProcess.stderr.on('data', (data) => {
-            socket.emit('pm2-log', data.toString());
-        });
-
-        socket.on('disconnect', () => {
-            logProcess.kill();
-        });
-    });
-
-    socket.on('disconnect', () => {
-        console.log('Client terputus');
-        clearInterval(interval);
+        }
     });
 });
 
-server.listen(PORT, () => {
-    console.log(`PM2 Monitor berjalan di http://localhost:${PORT}`);
+// Get logs for an app
+app.get('/api/logs/:name', (req, res) => {
+    const { name } = req.params;
+    const { spawn } = require('child_process');
+    const logStream = spawn('pm2', ['logs', name, '--lines', '50', '--nostream']);
+
+    let output = '';
+    logStream.stdout.on('data', data => output += data.toString());
+    logStream.stderr.on('data', data => output += data.toString());
+    logStream.on('close', () => res.send(`<pre>${output}</pre>`));
 });
+
+app.listen(PORT, () => console.log(`PM2 Panel running at http://localhost:${PORT}`));
